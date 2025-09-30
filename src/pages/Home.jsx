6b0e29a39f5e2713f6fff806841f4fb0836f61ca
Abdullah Amin <aminaminabdullah9@gmail.com>
2025-09-30 20:25:26 +0500

    solved problem Shop

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import Shop from './shop'
 import FeaturedCategory from '../components/FeaturedCategory'
+import Shop from './Shop'
 
 const Home = () => {
   return (
     <div className='flex flex-col  gap-4 text-2xl font-semibold'>
       <FeaturedCategory Features = 'Welcome To Home Page' Details='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore .'/>
-      <Shop />
+      <Shop/>
     </div>
   )
 }
