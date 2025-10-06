a817277d7dbf8f0f5978096122e2d14ec57c076b
Abdullah Amin <aminaminabdullah9@gmail.com>
2025-10-06 12:25:55 +0500

    review page  updated

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -17,7 +17,7 @@ const Review = () => {
                     </button>
                     <button
                         className={` bg-[#FFFFFF] w-[178px] cursor-pointer h-[50px] border-b-[4px] ${activeSection === 'additionalDetails' ? 'border-[#2F8EFF]' : 'border-transparent'} `}
-                    // onClick={() => setActiveSection('additionalDetails')}
+                    onClick={() => setActiveSection('additionalDetails')}
                     >
                         Additional Details
                     </button>
@@ -34,26 +34,28 @@ const Review = () => {
                             <p>The Search extension for the Tour Operators plugin adds robust search functionality to your WordPress Tour Operator site. It requires that you also have the FacetWP plugin installed, as that allows for much of the filtering functionality that the plugin.</p>
                         </div>
                     )}
-                    {/* {activeSection === 'additionalDetails' && ()} */}
+                    {activeSection === 'additionalDetails' && (
+                        <div className="h-[20px]"></div>
+                    )}
                     {activeSection === 'reviews' && (
                         <div className="review flex flex-col items-center" >
-                            <div className="border-b-[1px] border-[#B4B7C1] mb-[30px] w-[350px] lg:w-[640px]">
+                            <div className="border-b-[1px] border-[#B4B7C1] mb-[30px] w-[350px] md:w-[640px]">
                                 <p className='text-[24px]/[34px] font-bold mb-[20px]'>1 review for Subscription Product Title</p>
                             </div>
-                            <div className="userReview flex justify-center gap-[20px] mb-[40px] border-b-[1px] border-[#B4B7C1] pb-[30px]">
-                                <div className="userImg h-[50px] w-[50px] bg-black flex justify-center items-center">
-                                    <img src={userImg} alt="" />
+                            <div className="userReview md:w-[640px] w-[350px]  flex justify-center gap-[20px] mb-[40px] border-b-[1px] border-[#B4B7C1] pb-[30px]">
+                                <div className="userImg h-[50px] shrink-0 w-[50px] bg-black">
+                                    <img className='w-full h-full'src={userImg} alt="" />
                                 </div>
                                 <div className="reviewText">
-                                    <div className='text-[16px]/[24px] text-[#9B9B9B] font-medium flex justify-between items-center max-sm:flex-col gap-[20px] mb-[20px] md:gap-[10px] md:mb-[10px]'>
-                                        <p>lightspeed <span className='text-[#000000]'>(verified owner) </span>– 9 May 2023</p>
+                                    <div className='text-[16px]/[24px] text-[#9B9B9B] font-medium flex justify-between items-start max-md:flex-col gap-[20px] mb-[20px] md:gap-[10px] md:mb-[10px]'>
+                                        <p>lightspeed <span className='text-[#000000]'>(verified owner)</span> <br className='md:hidden'/><span className='max-md:hidden'> – </span>9 May 2023</p>
                                         <Rate value={3} disabled />
                                     </div>
                                     <p className='lg:w-[562px] text-[20px]/[30px] font-medium'>The LSX Tour Operator plugin is a comprehensive WordPress plugin designed specifically for tour operators and travel agencies. As a language model, I cannot provide personal opinion or experience with the product, but I can give you an overview of the plugin based on its features and user feedback.</p>
                                 </div>
-
                             </div>
-                            <div className='flex flex-col justify-center w-[350px] lg:w-[640px] '>
+
+                            <div className='flex flex-col justify-center md:w-[640px] w-[350px]'>
                                 <div className='font-medium mb-[30px]'>
                                     <p className='text-[24px]/[34px]'>Add a review</p>
                                     <p className='text-[16px]/[24px]'>Your email address will not be published. Required fields are marked <span className='text-[#FF0000]'>*</span></p>
@@ -70,11 +72,11 @@ const Review = () => {
                                         <textarea className='w-full h-[155px] bg-[#FFFFFF] border-[2px] px-[1px] border-[#2B2D2F] outline-none' id="yourReview" name="yourReview" />
                                     </div>
                                     <div className="name flex max-md:flex-col gap-[26px] mb-[40px]">
-                                        <div>
+                                        <div className='flex-1'>
                                             <label htmlFor="">First Name <span className='text-[#FF0000]'>*</span></label><br />
                                             <input className='w-full h-[55px] bg-[#FFFFFF] border-[2px] border-[#2B2D2F] px-[1px] outline-none' type="text" name="firstName" id="" />
                                         </div>
-                                        <div>
+                                        <div className='flex-1'>
                                             <label htmlFor="">Last Name <span className='text-[#FF0000]'>*</span></label><br />
                                             <input className='w-full h-[55px] bg-[#FFFFFF] border-[2px] border-[#2B2D2F] px-[1px] outline-none' type="text" name="lastName" id="" />
                                         </div>
