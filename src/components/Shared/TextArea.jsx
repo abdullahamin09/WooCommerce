08a389e7d7d3c56eadbcfd7d5d2407daf798b604
Abdullah Amin <aminaminabdullah9@gmail.com>
2025-10-17 12:21:52 +0500

    APIs hit

---
+++ src/components/Shared/TextArea.jsx
@@ -0,0 +1,19 @@
+
+const TextArea = ({
+    Label,
+    id,
+    Name,
+    important = false,
+    areaClass,
+    areaWrapClass = '',
+
+}) => {
+    return (
+        <div className={`flex flex-col items-start gap-[12px] mb-[10px] text-[20px]/[30px] font-medium ${areaWrapClass}`}>
+            <label className="" htmlFor={id}>{Label} {important && <span className='text-[#FF0000]'>*</span>}</label>
+            <textarea className={`border-[2px] border-[#000000] w-full h-[180px] resize-none font-normal p-[6px] focus:outline-none bg-white ${areaClass}`} required={important} name={Name} id={id}></textarea>
+        </div>
+    )
+}
+
+export default TextArea
